6faa06e7dd750ec8b85d2c666c542647e6a2221a
spivenko <spivenko@playtika.com>
2021-04-18 13:15:52 +0300

    Update the README file

--- src/test/java/com/spivenko/configurations/PetClinicTestConfiguration.java
+++ src/test/java/com/spivenko/configurations/PetClinicTestConfiguration.java
@@ -5,12 +5,9 @@ import com.google.inject.Provides;
 import com.spivenko.properties.PetClinicProperties;
 import org.aeonbits.owner.ConfigFactory;
 
-import static com.spivenko.environment.Environments.DOCKERISED;
-
 public class PetClinicTestConfiguration extends AbstractModule {
     @Provides
     static PetClinicProperties petClinicProperties() {
-        System.setProperty("env", DOCKERISED.name());
         return ConfigFactory.create(PetClinicProperties.class);
     }
 }
